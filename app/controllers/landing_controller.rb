197a31a157b3e6185d15d03c4d091b0e56e62378
Michael Kuehl <hello@txs.vc>
2020-08-19 10:09:10 +0200

    basic routing and removed old views

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,6 +1,7 @@
 class LandingController < ApplicationController
-
 	def index
+		if account_signed_in?
+			redirect_to "/dashboard"
+		end
 	end
-	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :accounts
 
+  get 'dashboard', to: 'dashboard#index'
+
   root 'landing#index'
 
 end
